5b548d1af6ea7d242beca160879d015822ecb1fa
wooooooak <wooooooak@gmail.com>
2019-04-19 20:41:01 +0900

    긴 함수를 하나의 목적을 가진 짧은 함수로 각각 분리 함수가 해야할 작업에 필요한 인수를 모두 명시하여 부수효과 개수를 줄임

--- Functional_Louis/part1.js
+++ Functional_Louis/part1.js
@@ -1,11 +1,19 @@
 // 부수효과를 일으키는 명령형 showStudent Function
-function showStudent(ssn) {
-    let student = db.fund(ssn); // 저장 객체에 접근해서 주어진 ssn으로 학생을 찾는다.
-    if (student !== null) {
-        document.querySelector(`#${elementId}`).innerHTML = `${student.ssn}, ${student.firstname}, ${student.firstname}` // 함수 밖에서 ID가 elementId인 요소를 찾음
-    } else {
-        throw new Error("학생을 찾을 수 없습니다!")
+const find = curry((db, id) => {
+    let obj = db.find(id);
+    if (obj === null) {
+        throw new Error("객체를 찾을 수 없습니다!");
     }
-}
+    return obj;
+})
 
-showStudent("111-11-11")
+const csv = student =>
+    `${student.ssn}, ${student.firsstname}, ${student.lastname}`;
+
+const append = curry((selector, info) => {
+    document.querySelector(selector).innerHTML = info
+})
+
+// 재사용 가능한 컴포넌트 3개로 나뉘어 코드가 훨씬 유연해짐
+// 이렇게 잘게 나뉜 함수를 재사용하면 신경 써서관리할 코드 크기가 확 줄기 때문에 생산성 향상
+// 가장 중요한 건, HTML 객체와의 상효작용을 자체 함수로 빼내어 순수하지 않은 불순한 로직을 순수함수에서 배제했다는 점.
